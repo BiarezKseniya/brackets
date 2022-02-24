4caee2b66a7f4ba8bb64b97d566621029b48cabc
Kseniya <ksenia-2010@tut.by>
2022-02-24 21:33:14 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+
+    for (const symbol of str) {
+        if (stack.length > 0) {
+            const last = stack[stack.length - 1];
+            if (getCloseTag(last, bracketsConfig) === symbol) {
+                stack.pop();
+            } else if (isTagOpening(symbol, bracketsConfig)) {
+                stack.push(symbol);
+            } else {
+                return false;
+            }
+        } else if (isTagOpening(symbol, bracketsConfig)) {
+            stack.push(symbol);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
 }
+
+function getCloseTag(openTag, bracketsConfig) {
+    const pair = bracketsConfig.find((pair) => {
+        return pair[0] === openTag;
+    });
+    return pair ? pair[1] : null;
+}
+
+function isTagOpening(tag, bracketsConfig) {
+    const pair = bracketsConfig.find((pair) => {
+        return pair[0] === tag;
+    });
+    return !!pair;
+}
+
